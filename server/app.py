20b980de5b3edf70c47c0766f70357cf6b6b444e
dann <austindan168@gmail.com>
2025-01-21 10:59:02 +0300

    REST lab

--- server/app.py
+++ server/app.py
@@ -46,10 +46,36 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self,id):
+        plant = Plant.query.filter_by(id=id).first()
+        data = request.get_json()
+        if not plant:
+            return {"error" : "Plant does not exist"}
+        if "name" in data:
+            plant.name = data['name']
+        if "image" in data:
+            plant.image = data['image']
+        if "price" in data:
+            plant.price = data['price']
+        if "is_in_stock" in data:
+            plant.is_in_stock = data['is_in_stock']
+        db.session.add(plant)
+        db.session.commit()
+
+        return make_response(jsonify(plant.to_dict()), 200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return {"error" : "plant not found!"}, 404
+        db.session.delete(plant)
+        db.session.commit()
+        return {}, 204
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555, debug=True) 
